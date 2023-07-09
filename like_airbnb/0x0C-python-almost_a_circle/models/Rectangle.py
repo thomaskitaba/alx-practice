d1c85b6c195c61e0da2eb0dcf25c4a8c9d033e44
thomaskitaba <thomaskitaba@gmail.com>
2023-07-09 14:50:19 +0300

    update practice folder

--- like_airbnb/0x0C-python-almost_a_circle/models/Rectangle.py
+++ like_airbnb/0x0C-python-almost_a_circle/models/Rectangle.py
@@ -10,7 +10,55 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
+
+    @property
+    def height(self):
+        return self.__width
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+                raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x muste be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y muste be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
 
 b1 = Rectangle(1, 2)  # 1
 print("b1: {}".format(b1.id))
