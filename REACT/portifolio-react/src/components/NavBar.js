3c3a9cc63e5c622526d6c95459f037b64d927b1d
thomaskitaba <thomas.kitaba@gmail.com>
2023-12-25 15:55:13 +0300

    update

--- REACT/portifolio-react/src/components/NavBar.js
+++ REACT/portifolio-react/src/components/NavBar.js
@@ -36,8 +36,9 @@ const NavBar = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#" className={activeLink === 'Reviews' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('Reviews')}>Reviews</Nav.Link>
+              <Nav.Link href="#Specials" className={activeLink === 'Specials' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('Specials')}>Specials</Nav.Link>
+
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
@@ -45,7 +46,7 @@ const NavBar = () => {
                 <a href="#2"><img src={navIcon2} alt="" /></a>
                 <a href="#3"><img src={navIcon3} alt="" /></a>
               </div>
-                <button className="vvd"><span>Let’s Connect</span></button>
+              <button className="vvd"><span>Let’s Connect</span></button>
             </span>
           </Navbar.Collapse>
         </Container>
